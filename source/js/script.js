f620c15764ddc3be4038667413fd085824bf24c2
mglushakova <glushakova7@mail.ru>
2018-02-01 10:48:17 +0500

    Задает микросетку блока Пример на главной странице

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,24 @@ toggleButton.addEventListener("click", function(event) {
     menu.classList.remove("main-nav--opened");
   }
 });
+
+var exampleImageBefore = document.querySelector(".example__image--fat");
+var exampleImageAfter = document.querySelector(".example__image--slim");
+var exampleButtonBefore = document.querySelector(".example__button--fat");
+var exampleButtonAfter = document.querySelector(".example__button--slim");
+
+exampleButtonBefore.addEventListener("click", function() {
+  exampleImageBefore.classList.add("example__image--active");
+  exampleImageAfter.classList.remove("example__image--active");
+
+  exampleButtonBefore.classList.add("example__button--active");
+  exampleButtonAfter.classList.remove("example__button--active");
+});
+
+exampleButtonAfter.addEventListener("click", function() {
+  exampleImageAfter.classList.add("example__image--active");
+  exampleImageBefore.classList.remove("example__image--active");
+
+  exampleButtonAfter.classList.add("example__button--active");
+  exampleButtonBefore.classList.remove("example__button--active");
+});
