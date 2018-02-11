4c0c3c17c7348e2015dcad2ba63ebeaa6e571ccc
mglushakova <glushakova7@mail.ru>
2018-02-11 10:50:13 +0500

    Исправляет подключение иконок во внешнем спрайте, подключает иконки на страницах каталога и формы

--- source/js/script.js
+++ source/js/script.js
@@ -39,3 +39,36 @@ if ( exampleButtonBefore && exampleButtonAfter ) {
   });
 }
 
+ymaps.ready(function () {
+  var myMap = new ymaps.Map("map", {
+      center: [59.938631, 30.323055],
+      zoom: 16
+    }, {
+      searchControlProvider: "yandex#search"
+    }),
+
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: "Собственный значок метки",
+      balloonContent: "Это красивая метка"
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: "default#image",
+      // Своё изображение иконки метки.
+      iconImageHref: "../img/map-pin.png",
+      // Размеры метки.
+      iconImageSize: [59, 53],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-20, -60]
+    });
+
+  myMap.geoObjects
+    .add(myPlacemark);
+});
+
